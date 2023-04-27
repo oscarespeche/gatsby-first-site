8cbb5fd5ee9763687da5cb9f55d6854e6b4697fe
oscar <orespeche@hotmail.com>
2023-04-27 14:05:01 -0300

    Finished Gatsby Tutorial Part 6

---
+++ gatsby-config - Copy.js
@@ -0,0 +1,36 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [
+  "gatsby-plugin-image",
+  "gatsby-plugin-sitemap",
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, 
+  "gatsby-plugin-sharp",
+  "gatsby-transformer-sharp", 
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  },
+  {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+]
+};
