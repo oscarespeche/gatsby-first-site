61566c59f23d9201a44a36b26f6a67caa22d7d5f
oscar <orespeche@hotmail.com>
2023-04-29 19:37:55 -0300

    uno mas

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from"../components/seo"
-
+import {StaticImage} from "gatsby-plugin-image"
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+	   <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/mel-elias-hhegBj6iJ5E-unsplash.jpg"
+      />
     </Layout>
   )
 }
